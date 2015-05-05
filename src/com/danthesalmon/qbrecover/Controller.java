549e771911bcb64749d1bbf2b91a12a566b1d841
Dan Salmon <dan.sa7mon@gmail.com>
2015-05-05 10:33:41 -0500

    Comment out FXML stuff for now. Create constructor method and test getOS()

--- src/com/danthesalmon/qbrecover/Controller.java
+++ src/com/danthesalmon/qbrecover/Controller.java
@@ -1,4 +1,34 @@
 package com.danthesalmon.qbrecover;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Controller {
+
+    //------------------------ VARIABLES -------------------------
+    private String strDrive = "";
+    private String strOS = "";
+    private String strVersionNum = "";
+
+
+
+    //------------------------ FUNCTIONS -------------------------
+
+    // Default constructor method
+    public Controller (String drive) {
+        strDrive = drive;
+
+        strOS = System.getProperty("os.name");
+        System.out.println("OS Found: " + strOS);
+       // Path xpFolder = Paths.get(strDrive + ":\");
+
+        // Determine which OS we are in based on the drive's folders (XP or 7/8/8.1)
+        //if (Files.exists(pathDrive)) {
+
+        //}
+
+    }
+
+
 }
--- src/com/danthesalmon/qbrecover/Main.java
+++ src/com/danthesalmon/qbrecover/Main.java
@@ -10,10 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        */
+        //primaryStage.show();
+
+        Controller c1 = new Controller("C");
+
     }
 
 
